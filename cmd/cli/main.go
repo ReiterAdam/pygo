6bb5f38c87da0ac6437b2af788beeb3193976d14
ReiterAdam <adamreiter@onmail.com>
2024-03-03 21:41:33 +0100

    Add venv setup command

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,17 +4,84 @@ import (
     "fmt"
     "log"
     "os"
+	"os/exec"
 
     "github.com/urfave/cli/v2"
 )
 
 func main() {
     app := &cli.App{
-        Name:  "boom",
-        Usage: "make an explosive entrance",
-        Action: func(*cli.Context) error {
-            fmt.Println("boom! I say!")
-            return nil
+        Commands: []*cli.Command{
+            {
+                Name:    "setup",
+                Aliases: []string{"s"},
+                Usage:   "create venv and project template",
+                Action: func(cCtx *cli.Context) error {
+					
+					// Check if venv folder is present
+
+					// Get the current working directory
+					dir, err := os.Getwd()
+					if err != nil {
+						return cli.Exit("Error getting current directory.", 83)
+					}
+
+					// Check if the "venv" folder exists in the current directory
+					_, err = os.Stat(dir + "/.venv")
+					if err == nil {
+						return cli.Exit("The 'venv' folder exists in the current directory.", 84)
+					}
+					if err != nil && !os.IsNotExist(err){
+						return cli.Exit("Error checking for 'venv' folder", 85)
+					}
+					
+					// Venv not found - create
+					// Prepare command 
+					cmd := exec.Command("python", "-m", "venv", ".venv")
+					
+					// Execute and check for errors
+					_, err = cmd.Output()
+					if err != nil {
+						return cli.Exit("Could not create new virtual environment", 86)
+					}
+					
+					// Confirm to user
+                    fmt.Println("Setup finished")
+                    return nil
+                },
+            },
+            {
+                Name:    "add",
+                Aliases: []string{"a"},
+                Usage:   "add a package to the venv and requirements",
+                Action: func(cCtx *cli.Context) error {
+                    fmt.Println("added package: ", cCtx.Args().First())
+                    return nil
+                },
+            },
+            {
+                Name:    "template",
+                Aliases: []string{"t"},
+                Usage:   "options for task templates",
+                Subcommands: []*cli.Command{
+                    {
+                        Name:  "add",
+                        Usage: "add a new template",
+                        Action: func(cCtx *cli.Context) error {
+                            fmt.Println("new task template: ", cCtx.Args().First())
+                            return nil
+                        },
+                    },
+                    {
+                        Name:  "remove",
+                        Usage: "remove an existing template",
+                        Action: func(cCtx *cli.Context) error {
+                            fmt.Println("removed task template: ", cCtx.Args().First())
+                            return nil
+                        },
+                    },
+                },
+            },
         },
     }
 
