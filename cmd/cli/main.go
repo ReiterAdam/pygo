6d18e659e52c76b46fd001c545b56f9d28430b04
ReiterAdam <adamreiter@onmail.com>
2024-03-12 23:08:08 +0100

    Further fixes in run command

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -10,28 +10,6 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-func is_venv() (bool, error) {
-	// Function checks if venv directory is made
-
-	// Get the current working directory
-	dir, err := os.Getwd()
-	if err != nil {
-		return false, err
-	}
-
-	// Check if the "venv" folder exists in the current directory
-	_, err = os.Stat(dir + "/.venv/")
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, err
-	}
-	// Return false if error is not due to not existence
-	return false, err
-
-}
-
 func main() {
 	app := &cli.App{
 		Commands: []*cli.Command{
@@ -42,19 +20,18 @@ func main() {
 				Action: func(cCtx *cli.Context) error {
 
 					// Check if venv folder is present
-					is_venv, _ := is_venv()
+					is_venv, _ := isVenv()
 					if is_venv {
 						return cli.Exit("Venv already here", 80)
 					}
 
 					// Venv not found - create
 					// Prepare command
-					cmd := exec.Command("python", "-m", "venv", ".venv")
+					cmdArgs := []string{"bash", "-c", "python -m venv .venv"}
+					fmt.Println(cmdArgs)
 
-					// Execute and check for errors
-					_, err := cmd.Output()
-					if err != nil {
-						return cli.Exit("Could not create new virtual environment", 82)
+					if err := executeCommand(cmdArgs); err != nil {
+						return cli.Exit("Could not setup venv", 82)
 					}
 
 					// Confirm to user
@@ -69,48 +46,29 @@ func main() {
 				Action: func(cCtx *cli.Context) error {
 
 					// Try to run program without venv
-					is_venv, _ := is_venv()
+					is_venv, _ := isVenv()
 					if !is_venv {
 						fmt.Println("Note, venv is not present.")
 					}
 					var cmdArgs []string
-					// source venv
-					if is_venv {
-						cmdArgs = []string{"bash", "-c", "source .venv/bin/activate"}
-						cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
-						_, err := cmd.Output()
-						if err != nil {
-							return cli.Exit("Could not source virtual environment", 90)
-						}
 
-						// add && for python script
-						cmdArgs = append(cmdArgs, "&&")
+					// Add venv sourcing
+					if is_venv {
+						cmdArgs = []string{"bash", "-c", "source .venv/bin/activate && python main.py"}
+					} else {
+						cmdArgs = []string{"python", "main.py"}
 					}
 
 					// Prepare command
+					// Get command line arguments from user
 					arguments := fmt.Sprint(cCtx.Args())
 					argumentsFmt := strings.Fields(arguments[2 : len(arguments)-1])
-					cmdArgs = append(cmdArgs, "python", "main.py")
+					// form whole command
 					cmdArgs = append(cmdArgs, argumentsFmt...)
-					cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
+					fmt.Println(cmdArgs)
 
-					// Set up pipes for interacting with the command
-					// Source: ChatGPT
-					cmd.Stdin = os.Stdin
-					cmd.Stdout = os.Stdout
-					cmd.Stderr = os.Stderr
-
-					fmt.Print("Running main.py...\n\n")
-					// Start the command
-					if err := cmd.Start(); err != nil {
-						fmt.Println("Error starting command:", err)
-						return cli.Exit("Could not start python program", 91)
-					}
-
-					// Wait for the command to finish
-					if err := cmd.Wait(); err != nil {
-						fmt.Println("Error waiting for command:", err)
-						return cli.Exit("Could not finish python program", 93)
+					if err := executeCommand(cmdArgs); err != nil {
+						return cli.Exit("Could not run program", 92)
 					}
 
 					fmt.Println("\nProgram finished.")
@@ -124,7 +82,7 @@ func main() {
 				Action: func(cCtx *cli.Context) error {
 
 					// Check if venv is present
-					is_venv, _ := is_venv()
+					is_venv, _ := isVenv()
 					if !is_venv {
 						return cli.Exit("Could not detect virtual environment", 100)
 					}
@@ -145,24 +103,10 @@ func main() {
 					// cmdArgs := append([]string{"-c", "pip", "install"}, argumentsFmt...)
 					cmdArgs = append(cmdArgs, "pip", "install")
 					cmdArgs = append(cmdArgs, argumentsFmt...)
-					cmd = exec.Command(cmdArgs[0], cmdArgs[1:]...)
 
-					// Set up pipes for interacting with the command
-					// Source: ChatGPT
-					cmd.Stdin = os.Stdin
-					cmd.Stdout = os.Stdout
-					cmd.Stderr = os.Stderr
-
-					// Start the command
-					if err := cmd.Start(); err != nil {
-						fmt.Println("Error starting command:", err)
-						return cli.Exit("Could not start python program", 102)
-					}
-
-					// Wait for the command to finish
-					if err := cmd.Wait(); err != nil {
-						fmt.Println("Error waiting for command:", err)
-						return cli.Exit("Could not finish python program", 103)
+					fmt.Println(cmdArgs)
+					if err := executeCommand(cmdArgs); err != nil {
+						return cli.Exit("Could not install package", 102)
 					}
 
 					fmt.Println("Added package: ", cCtx.Args().First())
@@ -198,4 +142,51 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func isVenv() (bool, error) {
+	// Function checks if venv directory is made
+
+	// Get the current working directory
+	dir, err := os.Getwd()
+	if err != nil {
+		return false, err
+	}
+
+	// Check if the "venv" folder exists in the current directory
+	_, err = os.Stat(dir + "/.venv/")
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, err
+	}
+	// Return false if error is not due to not existence
+	return false, err
+
+}
+
+func executeCommand(cmdArgs []string) error {
+
+	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
+
+	// Set up pipes for interacting with the command
+	// Source: ChatGPT
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Start the command
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error starting command:", err)
+		return err
+	}
+
+	// Wait for the command to finish
+	if err := cmd.Wait(); err != nil {
+		fmt.Println("Error waiting for command:", err)
+		return err
+	}
+
+	return nil
 }
